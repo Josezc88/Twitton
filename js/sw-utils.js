0e323808aacc158318eca5684fce653f21b453fb
Jose Luis <jluiscaldera@hotmail.com>
2019-04-08 18:23:42 -0500

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Guardar en el cache dinamico
+function actualizarCacheDinamico(dynamicCache, request, response) {
+    if (response.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(request, response.clone());
+            return response.clone();
+        });
+    } else {
+        return response;
+    }
+}
